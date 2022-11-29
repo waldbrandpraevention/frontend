9d0fa8fe810dcd9af602b3865a5cf41eed922a44
Adrian Schubek <adrian.schubek@gmail.com>
2022-11-29 02:40:26 +0100

    fix folder structure

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import logo from '../assets/img/logo.svg'
+import "../assets/styles/App.css"
 
 function App() {
   return (
