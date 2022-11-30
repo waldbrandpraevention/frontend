2c970241aa1302d996943639400fd51ef62f9dd5
Yannic <yannic.hochheimer@gmail.com>
2022-11-30 15:14:13 +0100

    fix css and add icons

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,6 +2,12 @@ import { useState } from "react";
 import { Nav } from "react-bootstrap";
 import '../assets/styles/sidebar.css'
 import Logo from "../assets/img/Logo"
+import { AiOutlinePieChart } from 'react-icons/ai'
+import { GrMap } from 'react-icons/gr'
+import { BsGearFill, BsListUl } from 'react-icons/bs'
+import { DiGoogleAnalytics } from 'react-icons/di'
+import { MdOutlinePrivacyTip } from 'react-icons/md'
+
 
 type SidebarProps = { visible?: boolean }
 
@@ -9,26 +15,29 @@ const Sidebar = (props: SidebarProps) => {
     const [visible] = useState(props.visible ?? true);
     return <> {visible && <Nav className="col-md-12 d-none d-md-block bg-light sidebar "
         activeKey="/home">
-        <Logo className="image"/>
+        <Logo className="image" />
         <div className="sidebar-sticky"></div>
         <Nav.Item>
-            <Nav.Link className="text-dark" href="/dashbord">Dashbord</Nav.Link>
+            <Nav.Link className="text-dark" href="/dashbord"><AiOutlinePieChart className="svg" /> Dashbord</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-            <Nav.Link className="text-dark" eventKey="zonenliste">Zonen</Nav.Link>
+            <Nav.Link className="text-dark" eventKey="zonenliste"><BsListUl className="svg" />Zonen</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-            <Nav.Link className="text-dark" eventKey="analyse">Analyse</Nav.Link>
+            <Nav.Link className="text-dark" eventKey="analyse"><DiGoogleAnalytics className="svg" />Analyse</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link className="text-dark" eventKey="map"><GrMap className="svg" />Karte</Nav.Link>
         </Nav.Item>
         <hr className="mt-5 mb-0"></hr>
         <Nav.Item>
-            <Nav.Link className="text-dark" eventKey="einstelungen">Einstelungen</Nav.Link>
+            <Nav.Link className="text-dark" eventKey="einstelungen"><BsGearFill className="svg" />Einstelungen</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-            <Nav.Link className="text-dark" eventKey="impressung">Impressung</Nav.Link>
+            <Nav.Link className="text-dark" eventKey="impressung"><BsGearFill className="svg" />Impressung</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-            <Nav.Link className="text-dark" eventKey="datenschutz">Datenschutz</Nav.Link>
+            <Nav.Link className="text-dark" eventKey="datenschutz"><MdOutlinePrivacyTip className="svg" />Datenschutz</Nav.Link>
         </Nav.Item>
     </Nav>}</>
 
