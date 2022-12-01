a48cc2cc62b80af6f3705894f530fb1c87c54b36
Yannic <yannic.hochheimer@gmail.com>
2022-12-01 16:12:54 +0100

    delete comment and unused import

--- src/components/bellIcon.tsx
+++ src/components/bellIcon.tsx
@@ -1,12 +1,10 @@
 import '../assets/styles/icon.css'
-import { useState } from "react";
 import { BiBell } from 'react-icons/bi'
 
 type BellIconProps = { visible?: boolean }
 
 
 const BellIcon = (props: BellIconProps) => {
-    //console.log(visible)
     return <>
         <BiBell className='Icon Bell'> </BiBell>
         {(props.visible ?? true) && <div className='Icon Notification'></div>}
