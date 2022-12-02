97151833732b8d95f5ab1fd8ff60849c3b22ace1
Adrian Schubek <adrian.schubek@gmail.com>
2022-12-02 18:18:54 +0100

    fix styling

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -8,7 +8,7 @@ type PanelProps = {
 /* Kacheln */
 const Tile = (props: PanelProps) => {
     /* Kacheln sollen aktiviert/deaktivert werden können */
-    return <Card className="my-1 py-3 shadow-sm" >
+    return <Card className="my-1 py-3 shadow-sm border-0" >
         {props.children}
     </Card>
 }
