356d6cd322d757fd3d97afef8ae5286e5a66e2bf
Adrian <adrian.schubek@gmail.com>
2022-12-06 19:39:08 +0100

    add sidebar links (#23)

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -9,6 +9,7 @@ import { DiGoogleAnalytics } from 'react-icons/di'
 import { MdOutlinePrivacyTip } from 'react-icons/md'
 import { ImSection, ImCross } from 'react-icons/im'
 import { FaBars } from 'react-icons/fa'
+import { Link } from "react-router-dom";
 
 
 
@@ -29,29 +30,29 @@ const Sidebar = () => {
     return <> {openMenu ? true && <Nav className="col-md-12 d-none d-md-block bg-light sidebar "
         activeKey="/home">
         <ImCross className="maximised" onClick={handleClick} />
-        <Logo className="image" />
+        <Logo as={Link} to="/" className="image" />
         <div className="sidebar-sticky"></div>
         <Nav.Item>
-            <Nav.Link className="text-dark" href="/dashbord"><AiOutlinePieChart className="svg" />Dashboard</Nav.Link>
+            <Nav.Link as={Link} to="/dashboard" className="text-dark"><AiOutlinePieChart className="svg" />Dashboard</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-            <Nav.Link className="text-dark" eventKey="zonenliste"><BsListUl className="svg" />Zonen</Nav.Link>
+            <Nav.Link as={Link} to="/zones" className="text-dark" eventKey="zonenliste"><BsListUl className="svg" />Zonen</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-            <Nav.Link className="text-dark" eventKey="analyse"><DiGoogleAnalytics className="svg" />Analyse</Nav.Link>
+            <Nav.Link as={Link} to="/advanced" className="text-dark" eventKey="analyse"><DiGoogleAnalytics className="svg" />Analyse</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-            <Nav.Link className="text-dark" eventKey="map"><GrMap className="svg" />Karte</Nav.Link>
+            <Nav.Link as={Link} to="/map" className="text-dark" eventKey="map"><GrMap className="svg" />Karte</Nav.Link>
         </Nav.Item>
         <hr className="mt-5 mb-0"></hr>
         <Nav.Item>
-            <Nav.Link className="text-dark" eventKey="einstelungen"><BsGearFill className="svg" />Einstellungen</Nav.Link>
+            <Nav.Link as={Link} to="/settings" className="text-dark" eventKey="einstellungen"><BsGearFill className="svg" />Einstellungen</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-            <Nav.Link className="text-dark" eventKey="impressung"><ImSection className="svg" />Impressum</Nav.Link>
+            <Nav.Link as={Link} to="/impressum" className="text-dark" eventKey="impressum"><ImSection className="svg" />Impressum</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-            <Nav.Link className="text-dark" eventKey="datenschutz"><MdOutlinePrivacyTip className="svg" />Datenschutz</Nav.Link>
+            <Nav.Link as={Link} to="/datenschutz" className="text-dark" eventKey="datenschutz"><MdOutlinePrivacyTip className="svg" />Datenschutz</Nav.Link>
         </Nav.Item>
     </Nav>
         : <FaBars className="minimised" onClick={handleClick} />}
