fdfd3514da953d53afe222a43836c4d9e30c86bf
Johanna Herbst <herbst_jo@web.de>
2022-12-10 13:45:59 +0100

    add Einsatzkräfte_alarmieren.tsx

---
+++ src/components/tiles/Einsatzkräfte_alarmieren.tsx
@@ -0,0 +1,69 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Card } from "react-bootstrap";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+function Einsatzkräfte_alarmieren() {
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <Card.Title>Einsatzkräfte alarmieren</Card.Title>
+          <Form>
+            <Form.Group as={Row} className="mb-3" controlId="Drohne">
+              <Form.Label column sm={2}>
+                Drohne:
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Control type="text" placeholder="Drohne-A123" />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="Einsatzort">
+              <Form.Label column sm={2}>
+                Einsatzort:
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Control type="text" placeholder="Zone-123" />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="Brandtyp">
+              <Form.Label column sm={2}>
+                Brandtyp:
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Control type="text" placeholder="Brandgefahr 123" />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="Notizen">
+              <Form.Label column sm={2}>
+                Notizen:
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  placeholder="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua"
+                />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formFileMultiple">
+              <Form.Label column sm={2}>
+                Anhänge hinzufügen (optional):{" "}
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Control type="file" multiple />
+              </Col>
+            </Form.Group>
+
+            <Button variant="danger" type="submit">
+              Alarmieren
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </>
+  );
+}
+
+export default Einsatzkräfte_alarmieren;
