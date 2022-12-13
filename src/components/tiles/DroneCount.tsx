5aec8751cf0bbc5f88ff1287f514e01ae3a22b7e
Adrian Schubek <adrian.schubek@gmail.com>
2022-12-13 20:29:09 +0100

    refresh dashboard alerts

--- src/components/tiles/DroneCount.tsx
+++ src/components/tiles/DroneCount.tsx
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { Alert } from "react-bootstrap";
-import { TbAlertTriangle } from "react-icons/tb";
+import ErrorAlert from "../alerts/ErrorAlert";
 import LoadingSpinner from "../LoadingSpinner";
 import Tile from "../Tile";
 
@@ -12,7 +11,7 @@ const DroneCount = () => {
 
     if (isLoading) return <Tile className="py-3" style={{alignItems: "center"}}><LoadingSpinner/></Tile>
 
-    if (isError) return <Alert key="danger" variant="danger"><TbAlertTriangle/> Drohnen konnten nicht geladen werden.</Alert>;
+    if (isError) return <ErrorAlert> Drohnen konnten nicht geladen werden.</ErrorAlert>;
 
     return <Tile className="py-3">
         Anzahl Drohnen {data.message}
--- src/components/tiles/ZoneOverview.tsx
+++ src/components/tiles/ZoneOverview.tsx
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { Alert } from "react-bootstrap";
-import { TbAlertTriangle } from "react-icons/tb";
+import ErrorAlert from "../alerts/ErrorAlert";
 import LoadingSpinner from "../LoadingSpinner";
 import Tile from "../Tile";
 
@@ -12,7 +11,7 @@ const ZoneOverview = () => {
 
     if (isLoading) return <Tile className="py-3" style={{alignItems: "center"}}><LoadingSpinner/></Tile>
 
-    if (isError) return <Alert key="danger" variant="danger"><TbAlertTriangle/> Zonenübersicht konnte nicht geladen werden.</Alert>;
+    if (isError) return <ErrorAlert> Zonenübersicht konnte nicht geladen werden.</ErrorAlert>;
 
     return <Tile className="py-3">
         Zonenübersicht {data.overview}
