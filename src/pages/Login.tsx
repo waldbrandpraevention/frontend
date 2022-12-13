ec57e6aac391c803924a9ba29116ef581d3f9813
Yannic <yannic.hochheimer@gmail.com>
2022-12-13 15:52:05 +0100

    add api

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,19 +3,51 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "../assets/styles/Login.css";
 import { loadingImages } from "../components/loadingImages.model"
-import { Card } from "react-bootstrap";
+import { Card, Alert } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useState } from "react";
+import LoadingSpinner from "../components/LoadingSpinner";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 
-const BgApp = styled.div`
+const BackgroundImage = styled.div`{
   background: url(${loadingImages[Math.floor(Math.random() * (loadingImages.length))]}) no-repeat center center fixed;
   background-size: cover !important;
-  
-`
+  width: 100%;
+  height: 100%;
+  z-index: -999999;
+  position: absolute;
+  filter: blur(2px);
+}`;
+
+type LoginFormData = {
+  email: string,
+  password: string
+}
 
 const Login = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: ""
+  } as LoginFormData);
+
+  const { isLoading, isError, isSuccess, mutate } = useMutation(["registrieren"], (data: LoginFormData) => {
+    return axios.post("https://httpbin.org/post", data).then(e => e.data); /* demo url */
+  });
+
+  const handleFormSubmit = (e: any) => {
+    e.preventDefault();
+    mutate(form);
+  }
+
+  const handleFormChange = (e: any) => {
+    e.preventDefault();
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
   return (
-    <BgApp className="App">
+    <div className="App">
+      <BackgroundImage></BackgroundImage>
       <div className="header"></div>
       <Card className="card-style">
         <Card.Body className="body-style">
@@ -28,27 +60,32 @@ const Login = () => {
           <Form >
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label className="label-style">Email Adresse</Form.Label>
-              <Form.Control type="email" placeholder="Email Adresse" />
+              <Form.Control type="email" placeholder="Email Adresse" name="email" value={form.email} onChange={handleFormChange} disabled={isLoading} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label className="label-style">Passwort</Form.Label>
-              <Form.Control type="password" placeholder="Passwort" />
+              <Form.Control className="mb-2" type="password" placeholder="Passwort bestätigen" name="password" value={form.password} onChange={handleFormChange} disabled={isLoading} />
               <Form.Label className="text-secondary" as={Link} to="/forgot-password" >Passwort vergessen</Form.Label>
             </Form.Group>
 
-            <Button variant="primary" type="submit">
-              Anmelden
+
+            <Button variant="primary" type="submit" disabled={isLoading}>
+              {isLoading ? <LoadingSpinner></LoadingSpinner> : <>Anmelden</>}
             </Button>
+
+            {isError && <Alert className="mt-2" variant="danger">Fehler :/.</Alert>}
+            {isSuccess && <Alert className="mt-2" variant="success" >Weiterleiten zu Dashbord</Alert>}
           </Form>
+
           <Card.Text className="text-style">
-            
+
             <Card.Link as={Link} to="/registrieren" >Registrieren</Card.Link>
           </Card.Text>
 
         </Card.Body>
       </Card>
-    </BgApp >
+    </div >
   );
 }
 
--- src/pages/Registrieren.tsx
+++ src/pages/Registrieren.tsx
@@ -2,18 +2,58 @@ import Logo from "../assets/img/Logo"
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "../assets/styles/Login.css";
-import { loadingImages } from "../components/loadingImages.model"
-import { Card } from "react-bootstrap";
+import { loadingImages } from "../components/loadingImages.model";
+import { Card, Alert } from "react-bootstrap";
 import styled from "styled-components";
+import { useState } from "react";
+import LoadingSpinner from "../components/LoadingSpinner";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 
-const BgApp = styled.div`
+const BackgroundImage = styled.div`
   background: url(${loadingImages[Math.floor(Math.random() * (loadingImages.length))]}) no-repeat center center fixed;
   background-size: cover !important;
+  width: 100%;
+  height: 100%;
+  z-index: -999999;
+  position: absolute;
+  filter: blur(2px);
 `
 
+type RegistrierenFormData = {
+    vorname: string,
+    zuname: string,
+    username: string,
+    email: string,
+    password: string
+}
+
 const Registrieren = () => {
+    const [form, setForm] = useState({
+        vorname: "",
+        zuname: "",
+        username: "",
+        email: "",
+        password: ""
+    } as RegistrierenFormData);
+
+    const { isLoading, isError, isSuccess, mutate } = useMutation(["registrieren"], (data: RegistrierenFormData) => {
+        return axios.post("https://httpbin.org/post", data).then(e => e.data); /* demo url */
+    });
+
+    const handleFormSubmit = (e: any) => {
+        e.preventDefault();
+        mutate(form);
+    }
+
+    const handleFormChange = (e: any) => {
+        e.preventDefault();
+        setForm({ ...form, [e.target.name]: e.target.value });
+    }
+
     return (
-        <BgApp className="App">
+        <div className="App">
+            <BackgroundImage></BackgroundImage>
             <div className="header"></div>
             <Card className="card-style">
                 <Card.Body className="body-style">
@@ -23,34 +63,37 @@ const Registrieren = () => {
                     <Card.Text className="text-style">
                         Bitte geben Sie ihre Informationen an
                     </Card.Text>
-                    <Form >
+                    <Form onSubmit={handleFormSubmit}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label className="label-style">Name</Form.Label>
-                            <Form.Control className="mb-2" type="name" placeholder="Vorname" />
-                            <Form.Control className="mb-2" type="name" placeholder="Nachname" />
+                            <Form.Control className="mb-2" type="text" placeholder="Vorname" name="vorname" value={form.vorname} onChange={handleFormChange} disabled={isLoading} />
+                            <Form.Control className="mb-2" type="text" placeholder="Nachname" name="zuname" value={form.zuname} onChange={handleFormChange} disabled={isLoading} />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label className="label-style">Benutzername</Form.Label>
-                            <Form.Control type="name" placeholder="Benutzername" />
+                            <Form.Control type="text" placeholder="Benutzername" name="username" value={form.username} onChange={handleFormChange} disabled={isLoading} />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label className="label-style">Email Addresse</Form.Label>
-                            <Form.Control type="email" placeholder="Email Adresse" />
+                            <Form.Control type="email" placeholder="Email Adresse" name="email" value={form.email} onChange={handleFormChange} disabled={isLoading} />
                         </Form.Group>
 
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label className="label-style">Passwort</Form.Label>
                             <Form.Control className="mb-2" type="password" placeholder="Passwort" />
-                            <Form.Control className="mb-2" type="password" placeholder="Passwort bestätigen" />
+                            <Form.Control className="mb-2" type="password" placeholder="Passwort bestätigen" name="password" value={form.password} onChange={handleFormChange} disabled={isLoading} />
                         </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Registrieren
+                        <Button variant="primary" type="submit" disabled={isLoading}>
+                            {isLoading ? <LoadingSpinner></LoadingSpinner> : <>Registrieren</>}
                         </Button>
+
+                        {isError && <Alert className="mt-2" variant="danger">Fehler :/.</Alert>}
+                        {isSuccess && <Alert className="mt-2" variant="success">Sie wurden Erfolgreich Registriert bitte bestätigen Sie ihre E-Mail um Fortzufahren</Alert>}
                     </Form>
                 </Card.Body>
             </Card>
-        </BgApp >
+        </div >
     );
 }
 
