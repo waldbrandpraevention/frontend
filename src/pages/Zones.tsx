6f33e77aab4e3097e07ee29be3595054a0751dd1
Yannic <yannic.hochheimer@gmail.com>
2022-12-20 15:08:55 +0100

    add api state

--- src/pages/Zones.tsx
+++ src/pages/Zones.tsx
@@ -1,64 +1,51 @@
-import { useState } from "react";
-import { Table } from "react-bootstrap";
+
+import { Container, Table } from "react-bootstrap";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import ErrorAlert from "../components/alerts/ErrorAlert";
+import Tile from "../components/Tile";
+import LoadingTile from "../components/tiles/LoadingTile";
 
 
-type ZonesFormData = {
-    zone: string,
-    drones: string,
-    lastUpadte: string,
-    DWDAssessment: string,
-    AIAssessment: string
-}
 
+const Zonen = () => {
+    //    const { data, isLoading, isError } = useQuery(["zones"], () => {
+    //         return axios.get("/zones").then(e => e.data);
+    //     });
 
-const Dashboard = () => {
-    const [form, setForm] = useState({
-        zone: "",
-        drones: "",
-        lastUpadte: "",
-        DWDAssessment: "",
-        AIAssessment: ""
-    } as ZonesFormData);
+    //     if (isLoading) return <LoadingTile />
 
+    //     if (isError) return <ErrorAlert> Überwachungsgebiet konnte nicht geladen werden.</ErrorAlert>;
+
+    const data = [{ zone: "string", drohne: "string", lastUpdate: "string", DangerLevel: 1, ai: 1 }]
     return (
-        <div className="app">
-            <Table className="table mx-lg-5">
-                <thead>
-                    <tr>
-                        <th scope="col">Zonen</th>
-                        <th scope="col">Drohnen</th>
-                        <th scope="col">Lestes Update</th>
-                        <th scope="col">Brandgefahreinschatzung</th>
-                        <th scope="col">KI Einschatzung</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry the Bird</td>
-                        <td>@twitter</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                    </tr>
-                </tbody>
-            </Table>
-
-        </div >
+        <Container className="mt-4">
+            <Tile >
+                <Table className="table mx-lg-5">
+                    <thead>
+                        <tr>
+                            <th scope="col">Zonen</th>
+                            <th scope="col">Drohnen</th>
+                            <th scope="col">Lestes Update</th>
+                            <th scope="col">Brandgefahreinschatzung</th>
+                            <th scope="col">KI Einschatzung</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((item: { zone: string; drohne: string; lastUpdate: string; DangerLevel: number; ai: number; }) => (
+                            <tr >
+                                <td >{item.zone}</td>
+                                <td >{item.drohne}</td>
+                                <td >{item.lastUpdate}</td>
+                                <td >{item.DangerLevel}</td>
+                                <td >{item.ai}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </Tile >
+        </Container>
     );
 }
 
-export default Dashboard;
+export default Zonen;
