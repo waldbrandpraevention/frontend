b9d38caa7746a5797487715efce63f67e6da3620
Yannic <yannic.hochheimer@gmail.com>
2022-12-21 12:37:35 +0100

    fix dangerlevel and refactor component

--- src/components/DangerLevel.tsx
+++ src/components/DangerLevel.tsx
@@ -1,39 +1,51 @@
 import Badge from 'react-bootstrap/Badge';
+import styled from 'styled-components';
+
+
+
+const Mydiv = styled.div`
+    min-width: 4rem;
+    max-width: 8rem;
+    border-radius: 25px;
+    text-align: center;
+    font-weight: 700;
+    filter: drop-shadow(1px 1px 1px black);
+`
 
 type DangerLevelProps = {
     level: number
 }
 
 const DangerLevel = (props: DangerLevelProps) => {
-    let backgroundColor;
+    let background;
     let text;
 
     switch (props.level) {
         case 0:
-            backgroundColor = '#00897B';
+            background = '#00897B';
             text = "SEHR NIEDRIG";
             break;
         case 1:
-            backgroundColor = "#4CAF50";
+            background = "#4CAF50";
             text = 'NIEDRIG';
             break;
         case 2:
-            backgroundColor = '#FFC107';
+            background = '#FFC107';
             text = 'MITTEL';
             break;
         case 3:
-            backgroundColor = '#F44336';
+            background = '#F44336';
             text = 'HOCH';
             break;
         case 4:
-            backgroundColor = '#C62828';
+            background = '#C62828';
             text = 'SEHR HOCH';
             break;
         default:
-            backgroundColor = 'gray';
+            background = 'gray';
             text = 'no data';
     }
 
-    return <Badge pill text="dark">{text}</Badge>
+    return <Mydiv style={{ backgroundColor: background }} > {text}</Mydiv >
 }
 export default DangerLevel;
--- src/pages/Zones.tsx
+++ src/pages/Zones.tsx
@@ -1,50 +1,14 @@
 
-import { Container, Table } from "react-bootstrap";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import ErrorAlert from "../components/alerts/ErrorAlert";
-import Tile from "../components/Tile";
-import LoadingTile from "../components/tiles/LoadingTile";
+import { Container } from "react-bootstrap";
+import ZoneOverview from "../components/tiles/ZoneOverview";
 
 
 
 const Zonen = () => {
-    //    const { data, isLoading, isError } = useQuery(["zones"], () => {
-    //         return axios.get("/zones").then(e => e.data);
-    //     });
-
-    //     if (isLoading) return <LoadingTile />
-
-    //     if (isError) return <ErrorAlert> Überwachungsgebiet konnte nicht geladen werden.</ErrorAlert>;
-
-    const data = [{ zone: "string", drohne: "string", lastUpdate: "string", DangerLevel: 1, ai: 1 }]
     return (
         <Container className="mt-4">
-            <Tile >
-                <Table className="table mx-lg-5">
-                    <thead>
-                        <tr>
-                            <th scope="col">Zonen</th>
-                            <th scope="col">Drohnen</th>
-                            <th scope="col">Lestes Update</th>
-                            <th scope="col">Brandgefahreinschatzung</th>
-                            <th scope="col">KI Einschatzung</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((item: { zone: string; drohne: string; lastUpdate: string; DangerLevel: number; ai: number; }) => (
-                            <tr >
-                                <td >{item.zone}</td>
-                                <td >{item.drohne}</td>
-                                <td >{item.lastUpdate}</td>
-                                <td >{item.DangerLevel}</td>
-                                <td >{item.ai}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
-            </Tile >
-        </Container>
+            <ZoneOverview></ZoneOverview>
+        </Container >
     );
 }
 
