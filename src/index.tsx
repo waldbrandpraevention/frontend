d0ae4094ebb5410c9ad13f6c1d03fd6874c764fc
Johanna Herbst <herbst_jo@web.de>
2022-12-27 15:01:09 +0100

    add advanced page

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { ProSidebarProvider } from 'react-pro-sidebar';
-import apiClientService from './service/api-client.service';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { ProSidebarProvider } from "react-pro-sidebar";
+import apiClientService from "./service/api-client.service";
 
-import './assets/styles/bootstrap.scss';
-import App from './pages/App';
-import Datenschutz from './pages/Datenschutz';
-import Impressum from './pages/Impressum';
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Registrieren from './pages/Registrieren';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Account from './pages/Account';
-import Zones from './pages/Zones';
-import { AuthProvider } from './service/auth';
-import AuthRoute from './components/routes/AuthRoute';
-import GuestRoute from './components/routes/GuestRoute';
+import "./assets/styles/bootstrap.scss";
+import App from "./pages/App";
+import Datenschutz from "./pages/Datenschutz";
+import Impressum from "./pages/Impressum";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Registrieren from "./pages/Registrieren";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Account from "./pages/Account";
+import Zones from "./pages/Zones";
+import { AuthProvider } from "./service/auth";
+import AuthRoute from "./components/routes/AuthRoute";
+import GuestRoute from "./components/routes/GuestRoute";
+import Advanced from "./pages/Advanced";
 
 const queryClient = new QueryClient(); // react-query config
 
 (async function index() {
-  await apiClientService.configureClient()
+  await apiClientService.configureClient();
 
   render(
     <QueryClientProvider client={queryClient}>
@@ -29,15 +30,39 @@ const queryClient = new QueryClient(); // react-query config
         <BrowserRouter>
           <AuthProvider>
             <Routes>
-              <Route path="/login" element={<GuestRoute><Login /></GuestRoute>} />
-              <Route path="/register" element={<GuestRoute><Registrieren /></GuestRoute>} />
-              <Route path="/" element={<AuthRoute><App /></AuthRoute>}>
-                <Route index element={<Navigate to={"/dashboard"} replace />}></Route>
+              <Route
+                path="/login"
+                element={
+                  <GuestRoute>
+                    <Login />
+                  </GuestRoute>
+                }
+              />
+              <Route
+                path="/register"
+                element={
+                  <GuestRoute>
+                    <Registrieren />
+                  </GuestRoute>
+                }
+              />
+              <Route
+                path="/"
+                element={
+                  <AuthRoute>
+                    <App />
+                  </AuthRoute>
+                }
+              >
+                <Route
+                  index
+                  element={<Navigate to={"/dashboard"} replace />}
+                ></Route>
                 <Route path="dashboard" element={<Dashboard />} />
                 <Route path="zones" element={<Zones />} />
                 <Route path="map" element={"test"} />
                 <Route path="help" element={"hilfeseite"} />
-                <Route path="advanced" element={<Impressum />} />
+                <Route path="advanced" element={<Advanced />} />
                 <Route path="datenschutz" element={<Datenschutz />} />
                 <Route path="impressum" element={<Impressum />} />
                 <Route path="settings">
@@ -51,13 +76,11 @@ const queryClient = new QueryClient(); // react-query config
         </BrowserRouter>
       </ProSidebarProvider>
     </QueryClientProvider>
-  )
-})()
+  );
+})();
 
 function render(element: JSX.Element) {
-  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-      {element}
-    </React.StrictMode>
+  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>{element}</React.StrictMode>
   );
 }
