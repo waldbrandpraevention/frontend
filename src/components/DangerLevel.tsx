0880ab53afbcf89e3afc9b87941daee86798451b
Adrian Schubek <adrian.schubek@gmail.com>
2023-01-06 23:38:45 +0100

    remove icon

--- src/components/DangerLevel.tsx
+++ src/components/DangerLevel.tsx
@@ -1,4 +1,4 @@
-import { TbAntennaBars1 as TbAntennaBars2, TbAntennaBars3, TbAntennaBars4, TbAntennaBars5 } from "react-icons/tb";
+import { TbAntennaBars1, TbAntennaBars1 as TbAntennaBars2, TbAntennaBars3, TbAntennaBars4, TbAntennaBars5 } from "react-icons/tb";
 import styled from 'styled-components';
 
 
@@ -21,38 +21,32 @@ const DangerLevel = (props: DangerLevelProps) => {
     let background;
     let text;
     let color;
-    let icon;
 
     switch (props.level) {
         case 0:
-            background = '#66BB6A';
+            background = '#81C784';
             color = '#1B5E20';
             text = "SEHR NIEDRIG";
-            icon = <TbAntennaBars2 />;
             break;
         case 1:
-            background = "#9CCC65";
+            background = "#AED581";
             color = '#33691E';
             text = 'NIEDRIG';
-            icon = <TbAntennaBars2 />;
             break;
         case 2:
             background = '#FFE082';
             color = '#F57F17';
             text = 'MITTEL';
-            icon = <TbAntennaBars3 />;
             break;
         case 3:
             background = '#FFAB91';
             color = '#E65100';
             text = 'HOCH';
-            icon = <TbAntennaBars4 />;
             break;
         case 4:
             background = '#EF9A9A';
             color = '#D32F2F';
             text = 'SEHR HOCH';
-            icon = <TbAntennaBars5 />;
             break;
         default:
             background = 'gray';
@@ -60,6 +54,6 @@ const DangerLevel = (props: DangerLevelProps) => {
             text = 'no data';
     }
 
-    return <Mydiv style={{ backgroundColor: background, color, filter: `drop-shadow(1px 1px 3px ${background})` }}> <span style={{ }}>{ icon }</span>{text}</Mydiv>
+    return <Mydiv style={{ backgroundColor: background, color, filter: `drop-shadow(1px 1px 3px ${background})` }}>{text}</Mydiv>
 }
         export default DangerLevel;
