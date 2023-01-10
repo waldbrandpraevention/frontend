b19ace1ae9273b64883885c64d56ab3162ed4345
Adrian Schubek <adrian.schubek@gmail.com>
2023-01-10 19:04:05 +0100

    add invite & edit form

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,18 +1,62 @@
-import { SetStateAction, useState } from "react"
+import { useMutation } from "@tanstack/react-query"
+import axios from "axios"
+import { useState } from "react"
 import { Badge, Button, Card, Col, Collapse, Container, Form, InputGroup, Modal, OverlayTrigger, Row, Table, Tooltip } from "react-bootstrap"
-import { TbAlertTriangle, TbBan, TbCheck, TbCopy, TbLink, TbMailFast, TbTrashX, TbUserPlus } from "react-icons/tb"
+import { TbAlertTriangle, TbArrowBack, TbCheck, TbCopy, TbLink, TbMailFast, TbTrashX, TbUserPlus } from "react-icons/tb"
+import { toast } from "react-toastify"
+import LoadingSpinner from "../components/LoadingSpinner"
 import { Account, AccountType } from "../service/auth"
 
 const dummyData: Account[] = [
   { firstname: "foo", lastname: "bar", mail: "foo@bar.de", organization: "KIWA", mail_verified: true, permission: 2, disabled: false, isAdmin: true, isUser: false },
-  { firstname: "foo", lastname: "bar", mail: "foo1@bar.de", organization: "KIWA", mail_verified: false, permission: 1, disabled: false, isAdmin: false, isUser: true },
-  { firstname: "foo", lastname: "bar", mail: "foo2@bar.de", organization: "KIWA", mail_verified: false, permission: 1, disabled: true, isAdmin: false, isUser: true },
+  { firstname: "Max", lastname: "Mustermann", mail: "max@mustermann.de", organization: "KIWA", mail_verified: false, permission: 1, disabled: false, isAdmin: false, isUser: true },
+  { firstname: "Anakin", lastname: "Skywalker", mail: "anakin@skywalker.de", organization: "KIWA", mail_verified: false, permission: 1, disabled: true, isAdmin: false, isUser: true },
 ]
 
-const CreateUserModal = ({ show, handleClose }: { show: boolean, handleClose: () => void }) => {
-  const onCreate = () => {
+type CreateUserFormData = {
+  email?: string,
+  organization: string,
+  permission: AccountType,
+  activateNow: boolean
+}
 
+const CreateUserModal = ({ show, handleClose }: { show: boolean, handleClose: () => void }) => {
+  const [form, setForm] = useState({
+    email: "",
+    organization: "",
+    permission: 1,
+    activateNow: true
+  } as CreateUserFormData);
+
+  const { isLoading: isLoadingEmail, mutate: mutateEmail } = useMutation(["users", "invite", "email"], async (data: CreateUserFormData) => {
+    console.log(data);
+    return axios.post("/users/invite/email/", data).then((e) => e.data);
+  }, {
+    onError(e: any) {
+      // setErrors(e.response.data.detail)
+      toast.error("Benutzer konnte nicht eingeladen werden.")
+    },
+    onSuccess() {
+      toast.success("Benutzer wurde erfolgreich eingeladen.")
+    }
+  });
+
+  const { isLoading: isLoadingLink, mutate: mutateLink } = useMutation(["users", "invite", "link"], async (data: CreateUserFormData) => {
+    return axios.post("/users/invite/link/", data).then((e) => e.data);
+  }, {
+    onError(e: any) {
+      // setErrors(e.response.data.detail)
+      toast.error("Einladungslink konnte nicht erstellt werden.")
+    },
+    onSuccess() {
+      toast.success("Einladungslink wurde erstellt.")
+    }
+  });
+
+  const handleFormChange = (e: any) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
   }
+
   return <Modal
     show={show}
     onHide={handleClose}
@@ -28,40 +72,49 @@ const CreateUserModal = ({ show, handleClose }: { show: boolean, handleClose: ()
       <Form>
         <Form.Group className="mb-3" >
           <Form.Label>E-Mail Adresse</Form.Label>
-          <Form.Control type="email" placeholder="" />
+          <Form.Control disabled={isLoadingEmail || isLoadingLink} type="email" placeholder="" name="email" value={form.email} onChange={handleFormChange} />
           <Form.Text className="text-muted">
             Nur notwendig bei E-Mail Einladung. Ein Einladungslink wird an diese E-Mail Adresse gesendet.
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Organisation</Form.Label>
-          <Form.Control type="text" placeholder="" />
+          <Form.Control disabled={isLoadingEmail || isLoadingLink} type="text" placeholder="" name="organization" value={form.organization} onChange={handleFormChange} />
           <Form.Text className="text-muted">
             Der Benutzer wird dieser Organisaton beitreten.
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" >
           <Form.Label>Rolle</Form.Label>
-          <Form.Select aria-label="Default select example" >
+          <Form.Select disabled={isLoadingEmail || isLoadingLink} aria-label="Default select example" name="permission" value={form.permission} onChange={handleFormChange} >
             {(Object.keys(AccountType) as Array<keyof typeof AccountType>).filter(v => isNaN(v as unknown as number)).map((key) =>
-              <option value={AccountType[key]}>{key}</option>)}
+              <option key={key} value={AccountType[key]}>{key}</option>)}
           </Form.Select>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className="mb-3">
           <Form.Check
             type="switch"
-            id="custom-switch"
-            checked={true}
             label="Account sofort aktivieren"
+            name="activateNow"
+            disabled={isLoadingEmail || isLoadingLink}
+            checked={form.activateNow}
+            onChange={() => setForm(old => ({ ...old, activateNow: !old.activateNow }))}
           />
+          <Form.Text className="text-muted">
+            Andernfalls wird der Account im Gesperrt-Status erstellt und muss erst nach Registrierung durch einen Administrator aktiviert werden.
+          </Form.Text>
         </Form.Group>
         <div className="d-grid gap-2 mt-2">
-          <Button className="d-flex align-items-center justify-content-center" variant="success" onClick={onCreate}><TbMailFast /> Einladen per E-Mail</Button>
+          <Button disabled={isLoadingEmail} className="d-flex align-items-center justify-content-center" variant="success" onClick={() => mutateEmail(form)}>
+            {!isLoadingEmail ? <><TbMailFast /> Einladen per E-Mail</> : <LoadingSpinner />}
+          </Button>
         </div>
         <hr />
         Alternativ können Sie einen Einladungslink erstellen um eine Person einzuladen. Dieser Link ist nur <b>einmalig</b> gültig.
         <div className="d-grid gap-2 mt-2">
-          <Button className="d-flex align-items-center justify-content-center" variant="success" onClick={onCreate}><TbLink /> Einladungslink generieren</Button>
+          <Button disabled={isLoadingLink} className="d-flex align-items-center justify-content-center" variant="success" onClick={() => mutateLink(form)}>
+            {!isLoadingLink ? <> <TbLink /> Einladungslink generieren</> : <LoadingSpinner />}
+          </Button>
         </div>
         <InputGroup className="mt-2" >
           <Form.Control disabled type="text" placeholder="" />
@@ -77,9 +130,59 @@ const CreateUserModal = ({ show, handleClose }: { show: boolean, handleClose: ()
 
 const EditUserModal = ({ show, user, handleClose }: { show: boolean, user: Account, handleClose: () => void }) => {
   const [open, setOpen] = useState(false);
-  const onCreate = () => {
 
+  const [form, setForm] = useState(/* {
+    firstname: "",
+    lastname: "",
+    username: "",
+    email: "",
+    password: "",
+    disabled: false,
+    permission: 1,
+  } */ user as Partial<Account>);
+
+  const { isLoading: isLoadingEdit, mutate: mutateEdit } = useMutation(["users", "edit"], async (data: Partial<Account>) => {
+    return axios.post("/users/edit/", data).then((e) => e.data);
+  }, {
+    onError(e: any) {
+      // setErrors(e.response.data.detail)
+      toast.error("Änderungen konnten nicht gespeichert werden.")
+    },
+    onSuccess() {
+      toast.success("Änderungen wurden gespeichert.")
+    }
+  });
+
+  const { isLoading: isLoadingDelete, mutate: mutateDelete } = useMutation(["users", "edit"], async (data: Partial<Account>) => {
+    return axios.post("/users/delete/", data).then((e) => e.data);
+  }, {
+    onError(e: any) {
+      // setErrors(e.response.data.detail)
+      toast.error("Benutzer konnte nicht gelöscht werden.")
+    },
+    onSuccess() {
+      toast.success("Benutzer wurde gelöscht.")
+    }
+  });
+
+  const { isLoading: isLoadingResetPw, mutate: mutateResetPw } = useMutation(["users", "edit"], async (data: Partial<Account>) => {
+    return axios.post("/users/reset-password/", data).then((e) => e.data);
+  }, {
+    onError(e: any) {
+      // setErrors(e.response.data.detail)
+      toast.error("Passwort konnte nicht zurückgesetzt werden.")
+    },
+    onSuccess() {
+      toast.success("Ein Link zum Passwort zurücksetzen wurde an die hinterlegte E-Mail Adresse gesendet.")
+    }
+  });
+
+  const handleFormChange = (e: any) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
   }
+
+  const isLoading = isLoadingDelete || isLoadingEdit || isLoadingResetPw
+
   return <Modal
     show={show}
     onHide={handleClose}
@@ -95,28 +198,41 @@ const EditUserModal = ({ show, user, handleClose }: { show: boolean, user: Accou
       <Form>
         <Form.Group className="mb-3">
           <Form.Label>Vorname</Form.Label>
-          <Form.Control type="text" value={user.firstname} />
+          <Form.Control disabled={isLoading} type="text" name="firstname" value={user.firstname} onChange={handleFormChange}/>
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Nachname</Form.Label>
-          <Form.Control type="text" value={user.lastname} />
+          <Form.Control disabled={isLoading} type="text" name="lastname" value={user.lastname} onChange={handleFormChange}/>
         </Form.Group>
         <Form.Label>E-Mail Adresse</Form.Label>
         <InputGroup className="mb-3">
-          <Form.Control type="email" value={user.mail} />
+          <Form.Control disabled={isLoading} type="email" name="mail" value={user.mail} onChange={handleFormChange}/>
           {!user.mail_verified ? <Badge className="d-flex align-items-center" bg="warning">unverifiziert</Badge> :
             <Badge className="d-flex align-items-center" bg="success">verifiziert</Badge>}
         </InputGroup>
         <Form.Group className="mb-3">
           <Form.Label>Organisation</Form.Label>
-          <Form.Control type="text" value={user.organization} />
+          <Form.Control disabled={isLoading} type="text" name="organization" value={user.organization} onChange={handleFormChange}/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Rolle</Form.Label>
-          <Form.Select aria-label="Default select example">
+          <Form.Select disabled={isLoading} aria-label="Default select example"  name="permission" value={form.permission} onChange={handleFormChange}>
             {(Object.keys(AccountType) as Array<keyof typeof AccountType>).filter(v => isNaN(v as unknown as number)).map((key) =>
-              <option selected={AccountType[key] === user.permission} value={AccountType[key]}>{key}</option>)}
+              <option key={key} selected={AccountType[key] === user.permission} value={AccountType[key]}>{key}</option>)}
           </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Check
+            type="switch"
+            label="Account gesperrt"
+            name="disabled"
+            disabled={isLoading}
+            checked={form.disabled}
+            onChange={() => setForm(old => ({ ...old, disabled: !old.disabled }))}
+          />
+          <Form.Text className="text-muted">
+            Der Account ist aktuell {form.disabled ? "deaktiviert. Anmeldung ist nicht mehr möglich." : "aktiviert."}
+          </Form.Text>
         </Form.Group>
         <div className="d-grid gap-2 mt-2">
           <Button className="d-flex align-items-center justify-content-center" variant="light" onClick={() => setOpen(o => !o)}>
@@ -127,12 +243,12 @@ const EditUserModal = ({ show, user, handleClose }: { show: boolean, user: Accou
           <Row className="mt-2">
             <Col>
               <div className="d-grid ">
-                <Button className="d-flex align-items-center justify-content-center" variant="outline-danger" onClick={onCreate}><TbBan /> Deaktivieren</Button>
+                <Button disabled={isLoading} className="d-flex align-items-center justify-content-center" variant="outline-danger" onClick={() => mutateResetPw(form)}><TbArrowBack /> Passwort zurücksetzen</Button>
               </div>
             </Col>
             <Col>
               <div className="d-grid ">
-                <Button className="d-flex align-items-center justify-content-center" variant="outline-danger" onClick={onCreate}><TbTrashX /> Entfernen</Button>
+                <Button disabled={isLoading} className="d-flex align-items-center justify-content-center" variant="outline-danger" onClick={() => mutateDelete(form)}><TbTrashX /> Entfernen</Button>
               </div>
             </Col>
           </Row>
@@ -140,10 +256,10 @@ const EditUserModal = ({ show, user, handleClose }: { show: boolean, user: Accou
       </Form>
     </Modal.Body>
     <Modal.Footer>
-      <Button variant="light" onClick={handleClose}>
+      <Button disabled={isLoading} variant="light" onClick={handleClose}>
         Abbrechen
       </Button>
-      <Button variant="success" onClick={handleClose} className="d-flex align-items-center">
+      <Button disabled={isLoading} variant="success" onClick={() => mutateEdit(form)} className="d-flex align-items-center">
         <TbCheck></TbCheck> Speichern
       </Button>
     </Modal.Footer>
@@ -153,30 +269,20 @@ const EditUserModal = ({ show, user, handleClose }: { show: boolean, user: Accou
 
 const Users = () => {
   const [user, setUser] = useState({} as Account)
-  const [show, setShow] = useState(false);
   const [newUser, setNewUser] = useState(false);
   const [editUser, setEditUser] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   return <Container>
-    {/* <Offcanvas show={show} onHide={handleClose} scroll={true} backdrop={false}>
-            <Offcanvas.Header closeButton>
-                <Offcanvas.Title>Offcanvas</Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-                Some text as placeholder. In real life you can have the elements you
-                have chosen. Like, text, images, lists, etc.
-            </Offcanvas.Body>
-        </Offcanvas> */}
-
     <CreateUserModal show={newUser} handleClose={() => setNewUser(false)}></CreateUserModal>
     <EditUserModal user={user} show={editUser} handleClose={() => { setUser({} as Account); setEditUser(false) }}></EditUserModal>
 
-    <Card body className="mt-2 my-1 shadow-sm border-0">
-      <Button className="d-flex align-items-center" variant="success" onClick={() => { setNewUser(true) }}><TbUserPlus /> Benutzer einladen</Button>
-    </Card>
+    {/* <Card body className="mt-2 my-1 shadow-sm border-0"> */}
+    <Row className="mt-2 my-1">
+      <div>
+        <Button className="d-flex align-items-center float-start" variant="success" onClick={() => { setNewUser(true) }}><TbUserPlus /> Benutzer einladen</Button>
+      </div>
+    </Row>
+    {/* </Card> */}
     <Row>
       <Col /* md={8} */>
         <Card body className="my-1 shadow-sm border-0">
