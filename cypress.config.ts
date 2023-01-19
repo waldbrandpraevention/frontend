41f00bf07c5b37e651def705ca44cf4247c95d7d
Adrian Schubek <adrian.schubek@gmail.com>
2023-01-19 18:53:50 +0100

    add cypress e2e testing

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config)
+
+      return config
+    },
+    experimentalStudio: true
+  },
+});
